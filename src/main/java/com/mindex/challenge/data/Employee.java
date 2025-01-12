4616fe8b2c48e394dccd4365ee8941f9187ceb07
Kavan Mally <kavan.mally@gmail.com>
2025-01-11 20:23:32 -0500

    added tag to hide missing values from Employee. Hiding it so direct reports field doesn't return unnecessary values from Employee object.

--- src/main/java/com/mindex/challenge/data/Employee.java
+++ src/main/java/com/mindex/challenge/data/Employee.java
@@ -1,10 +1,12 @@
 package com.mindex.challenge.data;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Employee {
     private String employeeId;
     private String firstName;
